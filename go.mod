61686368ac7167aae6a74f56e94eb0773b9708de
Antony Holmes <antony@antonybholmes.com>
2024-04-27 00:47:38 -0400

    fix: Bug fixes and updates.

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/antonybholmes/go-microarray
 
 go 1.22.2
+
+replace github.com/antonybholmes/go-sys => ../go-sys
+
+require github.com/antonybholmes/go-sys v0.0.0-20240222002015-d0dad7b0c431
+
+
+require github.com/mattn/go-sqlite3 v1.14.22 // indirect
---
+++ microarray.go
@@ -0,0 +1,27 @@
+package microarray
+
+import (
+	"database/sql"
+
+	"github.com/antonybholmes/go-sys"
+)
+
+const ALL_SAMPLES_SQL = `SELECT uuid, array, name
+	FROM samples ORDER BY array, name`
+
+type MicroarrayDB struct {
+	db             *sql.DB
+	allSamplesStmt *sql.Stmt
+}
+
+func NewMicroarrayDb(file string) (*MicroarrayDB, error) {
+	db := sys.Must(sql.Open("sqlite3", file))
+
+	return &MicroarrayDB{db: db,
+		allSamplesStmt: sys.Must(db.Prepare(ALL_SAMPLES_SQL)),
+	}, nil
+}
+
+func (genedb *MicroarrayDB) Close() {
+	genedb.db.Close()
+}
